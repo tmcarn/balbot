774666cf6908b7d81ddfff3083a64fa968e87f90
tmcarn <theo_mcarn@brown.edu>
2025-01-22 13:55:06 -0500

    update

--- motortest.py
+++ motortest.py
@@ -7,15 +7,14 @@ PIN = 26
 motor = PWMOutputDevice(PIN, frequency=50)
 
 # Full throttle
+print("Full throttle")
 motor.value = 0.1
 time.sleep(2)
 
-# Half throttle
-motor.value = 0.075
-time.sleep(2)
-
 # Low throttle
+print("Low throttle")
 motor.value = 0.05
 time.sleep(2)
 
 motor.value = 0
+print("All done")
