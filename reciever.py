074971fd2b091af6f3c9944b0fa55d490b604848
tmcarn <theo_mcarn@brown.edu>
2025-01-25 10:58:32 -0500

    library fix

--- reciever.py
+++ reciever.py
@@ -2,10 +2,9 @@ from flySkyiBus import IBus
 
 bus = IBus('/dev/ttyAMA10')  # use your serial port name here
 
-while True:
-    data = bus.read()  # Read data from serial port
-    print(data)  # print the data read from the serial port
 
-    print("running")
+data = bus.read()  # Read data from serial port
+print(data)  # print the data read from the serial port
+
 
     
