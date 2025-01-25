bae230e44108b9aa26f4e8632d10cbcbee63a7d3
tmcarn <theo_mcarn@brown.edu>
2025-01-25 12:07:35 -0500

    transmitter working?

--- reciever.py
+++ reciever.py
@@ -1,17 +1,17 @@
-# from flySkyiBus import IBus
+from flySkyiBus import IBus
 
 
-# bus = IBus('/dev/ttyAMA10')  # use your serial port name here
-
-
-# data = bus.read()  # Read data from serial port
-# print(data)  # print the data read from the serial port
-
-
-import serial
-
-ser = serial.Serial('/dev/ttyAMA0', baudrate=115200, timeout=1)
+bus = IBus('/dev/ttyAMA10')  # use your serial port name here
 
 while True:
-    data = ser.readline(32)
-    print(data)
+    data = bus.read()  # Read data from serial port
+    print(data)  # print the data read from the serial port
+
+
+# import serial
+
+# ser = serial.Serial('/dev/ttyAMA0', baudrate=115200, timeout=1)
+
+# while True:
+#     data = ser.readline(32)
+#     print(data)
