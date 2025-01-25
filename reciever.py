c9d352a81d10d507ef2bc758cece2acba0bf0411
tmcarn <theo_mcarn@brown.edu>
2025-01-25 11:06:40 -0500

    serial test

--- reciever.py
+++ reciever.py
@@ -1,10 +1,17 @@
-from flySkyiBus import IBus
-
-bus = IBus('/dev/ttyAMA10')  # use your serial port name here
+# from flySkyiBus import IBus
 
 
-data = bus.read()  # Read data from serial port
-print(data)  # print the data read from the serial port
+# bus = IBus('/dev/ttyAMA10')  # use your serial port name here
 
 
-    
+# data = bus.read()  # Read data from serial port
+# print(data)  # print the data read from the serial port
+
+
+import serial
+
+ser = serial.Serial('/dev/ttyAMA10', baudrate=115200, timeout=1)
+
+while True:
+    data = ser.readline(32)
+    print(data)
