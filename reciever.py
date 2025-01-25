1559c5a7e5fae3f2b5d438340a48e5615765b448
tmcarn <theo_mcarn@brown.edu>
2025-01-25 10:53:06 -0500

    library fix

--- reciever.py
+++ reciever.py
@@ -1,9 +1,6 @@
-import sys
-sys.path.append("/home/theo/FlySky-iBus/flySkyiBus")
-
 from flySkyiBus import IBus
 
-bus = IBus('/dev/serial0')  # use your serial port name here
+bus = IBus('/dev/ttyA-MA0')  # use your serial port name here
 
 while True:
     data = bus.read()  # Read data from serial port
