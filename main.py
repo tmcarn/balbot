ef31d5910d6224d0044b610aa3165e5ea2d415a4
tmcarn <theo_mcarn@brown.edu>
2025-01-26 15:37:20 -0500

    fiximu

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ LOOP_DELAY = 0.01
 imu = IMU()
 
 motor1 = MotorController(PIN_1)
-# motor2 = MotorController(PIN_2)
+motor2 = MotorController(PIN_2)
 
 pid = PID()
 reciever = Steering_Controller()
@@ -26,29 +26,30 @@ prev_time = time.time()
 current_time = 0
 dt = 0
 
+running = True
+
 while True:
-    try:
-        pitch = imu.get_pitch() # in radians
+    pitch = imu.get_pitch() # in radians
 
-        if pitch == None:
-            print("No IMU Reading")
-            continue
-        
-        current_time = time.time()
-        dt = current_time - prev_time
-        prev_time = current_time
+    if pitch == None:
+        print("No IMU Reading")
+        continue
 
-        motor_value = pid.compute(pitch, dt)
-        motor1.set_motor_speed(motor_value)
-        # motor2.set_motor_speed(motor_value)
-        time.sleep(LOOP_DELAY)
+    elif np.abs(pitch)> 90:
+        break
+    
+    current_time = time.time()
+    dt = current_time - prev_time
+    prev_time = current_time
 
-    finally:
-        motor1.kill_motor()
-        # motor2.kill_motor()
-        print("Loop Terminated")
+    motor_value = pid.compute(pitch, dt)
+    motor1.set_motor_speed(motor_value)
+    motor2.set_motor_speed(motor_value)
+    time.sleep(LOOP_DELAY)
 
 
+motor1.kill_motor()
+print("Loop Terminated")
 
 
     
