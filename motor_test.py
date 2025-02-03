7e8a3442a6a22af30df81218bdc0ba68493ebced
tmcarn <theo_mcarn@brown.edu>
2025-02-02 20:38:05 -0500

    motor test

--- motor_test.py
+++ motor_test.py
@@ -1,13 +1,19 @@
-from gpiozero import Motor
-from gpiozero import PWMOutputDevice
+from gpiozero import Motor, PWMOutputDevice, OutputDevice
 
 import time
 
 motor = Motor(6, 13)
 pwm = PWMOutputDevice(26)
 
+stby = OutputDevice(5)
+
+print("Enabling motor (STBY HIGH)...")
+stby.on()  # Enable the motor driver
+
+print("Forward")
 motor.forward(0.5)
 time.sleep(2)
 
+print("Backwards")
 motor.backward()
 time.sleep(2)
