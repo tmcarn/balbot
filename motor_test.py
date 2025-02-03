b063c8f67d02ba5e75d7315951fbf5c00c019542
tmcarn <theo_mcarn@brown.edu>
2025-02-02 20:52:00 -0500

    motor test

--- motor_test.py
+++ motor_test.py
@@ -3,7 +3,7 @@ from gpiozero import Motor, PWMOutputDevice, OutputDevice
 import time
 
 motor = Motor(6, 13)
-pwm = PWMOutputDevice(26)
+pwm = PWMOutputDevice(26, frequency=100_000)
 
 stby = OutputDevice(5)
 
@@ -11,7 +11,5 @@ print("Enabling motor (STBY HIGH)...")
 stby.on()  # Enable the motor driver
 pwm.on()
 
-
-pwm.pulse(n=5)
-
 motor.forward()
+time.sleep(5)
