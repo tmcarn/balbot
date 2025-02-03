8c95325790acd8f3d96fa79623daa7bbba0fee22
tmcarn <theo_mcarn@brown.edu>
2025-02-03 13:03:43 -0500

    first full test

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ while running:
     dt = current_time - prev_time
     prev_time = current_time
 
-    pid.set_constants((0.1,0,0))
+    pid.set_constants((0.01,0,0))
     motor_value = pid.compute(pitch, dt)
 
     motors.update_motors(motor_value)
