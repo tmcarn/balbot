3d9d2e542c9c73051acf49e67e6fd9bb4b797d46
tmcarn <theo_mcarn@brown.edu>
2025-02-04 23:26:27 -0500

    PID TUNING

--- radio_controller.py
+++ radio_controller.py
@@ -7,7 +7,7 @@ class RadioController():
         self.bus = IBus('/dev/ttyAMA0')
 
     def raw_inputs(self):
-        self.bus.ser.reset_input_buffer()  # Flush old data
+        # self.bus.ser.reset_input_buffer()  # Flush old data
         data = np.array(self.bus.read())  # Read data from serial port
         print(f"DATA: {data}")
         return(data[2:8])  # Returns only data from our 6 channels
