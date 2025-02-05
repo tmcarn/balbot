8ffc4b23061dd5a414f08032555b0054bd3c76f2
tmcarn <theo_mcarn@brown.edu>
2025-02-04 23:07:32 -0500

    PID TUNING

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ while running:
     dt = current_time - prev_time
     prev_time = current_time
 
-    pid.set_constants((0.5,0,0.1))
+    pid.set_constants((0.07,0,0.01))
     motor_value = pid.compute(pitch, dt)
 
     motors.update_motors(motor_value)
